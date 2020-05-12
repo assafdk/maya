edd70d906a9ba86d6d526005139022b923fedabf
Assaf Dekel <assafdk@gmail.com>
2020-05-12 15:10:44 +0300

    add build TASE stocks lookup table

--- tase.py
+++ tase.py
@@ -1,17 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from datetime import datetime
+
+# Check URLs !!!
+# Declare TASE stock DB URLs
+HEB_TASE_STOCKS_URL = "https://info.tase.co.il/_layouts/15/Tase/ManagementPages/Export.aspx?sn=none&action=1&SubAction=0&GridId=33&CurGuid=%7B26F9CCE6-D184-43C6-BAB9-CF7848987BFF%7D&ExportType=3"
+ENG_TASE_STOCKS_URL = "https://info.tase.co.il/_layouts/15/Tase/ManagementPages/Export.aspx?sn=none&action=1&SubAction=0&GridId=33&CurGuid=%7B85603D39-703A-4619-97D9-CE9F16E27615%7D&ExportType=3"
 
 GLOBES_ALL_STOCKS_URL = "https://www.globes.co.il/portal/quotes/?showAll=true"
 GLOBES_STOCK_URL_FORMAT = "https://www.globes.co.il/portal/instrument.aspx?instrumentid={}"
 #TASE_STOCK_INFO_URL_FORMAT = "https://www.tase.co.il/{0}/market_data/security/{1}/major_data"
-
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
+
+
 def __init__(self, maya_msg_url):
     self.msg_url = maya_msg_url
 
 
-def get_stocks_dataframe_from_csv(csv_file):
+def get_stocks_df_from_tase(url, prefix_name=""):
+    response = requests.get(url)
+    dateTimeObj = str(datetime.now())
+    filename = prefix_name+"tase_stocks_"+dateTimeObj+".csv"
+    file = open(filename,"w")
+    file.write(response.text)
+    df = pd.read_csv(filename,skiprows=3)
+    return df
+
+
+def get_stocks_df_from_csv(csv_file):
     return pd.read_csv(csv_file)
 
 
@@ -92,17 +109,66 @@ def fetch_stock_details(self):
     soup = BeautifulSoup(self.msg_url, 'html.parser')
     return
 
-# # build stock dataframe from web
-# stocks_list = fetch_tase_stocks_list()
-# stocks_dict  =  {'Heb Name' : stocks_list[0], 'Stock Num' : stocks_list[1], 'Globes ID' : stocks_list[2]}
-# stocks_df = pd.DataFrame(stocks_dict)
-# #tickers = stocks_df['Globes ID'].apply(fetch_ticker)   # doesn't work
-# tickers_list = fetch_tickers_one_by_one(stocks_df)
-# stocks_df['Ticker'] = tickers_list
-# stocks_df.to_csv("stocks_df.csv", index=False, header=True)
+# # Not used
+# def build_stock_df_from_web():
+#     stocks_list = fetch_tase_stocks_list()
+#     stocks_dict  =  {'Heb Name' : stocks_list[0], 'Stock Num' : stocks_list[1], 'Globes ID' : stocks_list[2]}
+#     stocks_df = pd.DataFrame(stocks_dict)
+#     #tickers = stocks_df['Globes ID'].apply(fetch_ticker)   # doesn't work
+#     tickers_list = fetch_tickers_one_by_one(stocks_df)
+#     stocks_df['Ticker'] = tickers_list
+#     stocks_df.to_csv("stocks_df.csv", index=False, header=True)
+# 
+#     return stocks_df
 
-# load stored stock dataframe from csv
-stocks_df = get_stocks_dataframe_from_csv("stocks_df.csv")
+
+def load_stored_stock_df_from_csv():
+    stocks_df = get_stocks_df_from_csv("stocks_df.csv")
+
+
+def find_ISIN(heb_number,df):
+    ISIN = df[df.ISIN.str.extract("(" + heb_number + ')', expand=False).notnull()]['ISIN']
+    if ISIN.size > 0:
+        return ISIN.values[0]
+    return 'None'
+
+
+def get_xls_intraday_data(globes_id):
+    print("get_xls_intraday_data for instrument "+globes_id)
+    xlsurl  = "https://www.globes.co.il/portal/instrument/instrumentgraph_toexcel.aspx?instrumentid="+globes_id+"&feeder=0"
+    res = requests.get(xlsurl)
+    out = open('test1.xls','wb')
+    out.write(res.content)
+    out.close()
+    data = pd.read_html('test1.xls',skiprows=1)
+    data.info()
+    # TODO: Change headers and convert to DataFrame
+
+# Main
+def build_master_stock_df():
+    # Build Hebrew and English stock df
+    tlv_stocks_heb = get_stocks_df_from_tase(HEB_TASE_STOCKS_URL, prefix_name="heb")
+    tlv_stocks_heb = tlv_stocks_heb.iloc[:, :3]
+    tlv_stocks_heb.columns = ['heb_name','heb_symbol','number']
+    tlv_stocks_eng = get_stocks_df_from_tase(ENG_TASE_STOCKS_URL, prefix_name="eng")
+    tlv_stocks_eng = tlv_stocks_eng.iloc[:, :3]
+    tlv_stocks_eng.columns = ['eng_name','ticker','ISIN']
+    tlv_stocks_heb['number'] = tlv_stocks_heb['number'].astype(str)
+
+    # add ISIN column to Hebrew df
+    tlv_stocks_heb['ISIN'] = 'None'
+    for index,row in tlv_stocks_heb.iterrows():
+        tlv_stocks_heb.at[index, 'ISIN'] = find_ISIN(row['number'], tlv_stocks_eng)
+
+    # # check
+    # stocks_with_common_ISIN = tlv_stocks_heb[tlv_stocks_heb['ISIN'] != 'None']
+    # stocks_with_NO_common_ISIN = tlv_stocks_heb[tlv_stocks_heb['ISIN'] == 'None']
+
+    stocks_lookup_df = pd.merge(tlv_stocks_eng, tlv_stocks_heb, how='inner', on='ISIN', left_on=None, right_on=None,left_index=False, right_index=False, sort=True, suffixes=('', ''), copy=True, indicator=False,validate=None)
+    stocks_lookup_df.set_index('ticker')
+    print('Done - build_master_stock_df()')
+
+    return stocks_lookup_df
 
 #intra_day = fetch_intraday_data('373019', 'AURA')
 #print(intra_day.text)
