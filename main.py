881346485972974d90113919a93ec34d1567ff8c
Assaf Dekel <assafdk@gmail.com>
2020-06-10 00:30:04 +0300

    add mock IB skeleton to main

--- main.py
+++ main.py
@@ -1,49 +1,64 @@
-import rss
-import requests
-from datetime import datetime
+import datetime as dt
 import maya
 import tase
-
-
-def get_triggers():
-    # TODO: Define trigger
-    ticker = False
-    ticker = maya.check_trigger()
-    return ticker
+import time
 
 
 # Main
 #---DEBUG----
 # tase.get_historical_data(start_date=datetime(2020,4,1),end_date=datetime(2020,5,27))
-# print("done")
+# print("DEBUG")
 # dir_path = "/Users/assafdekel/other_projects/maya/temp"
 #tase.sort_all_stock_files_in_dir(dir_path)
 # tase.rename_files_in_dir(dir_path)
-#------------
 
-# Build TASE stocks lookup table
-stocks_df = tase.build_master_stock_df()
 # Get & append new intra_day data
 # intraday_dir_path l= '/Users/assafdekel/other_projects/maya/intraday'
 # tase.get_all_todays_intraday_to_files(stocks_df,dir_path=intraday_dir_path)
+#------------
 
-# Retrieve MAYA messages
-maya_msgs = maya.create_msgs_dataframe(stocks_df)
-# Get sentiment for each message
-maya_msgs = maya.analyze(maya_msgs)
+# ------------ init ------------
+# Build TASE stocks lookup table
+stocks_df = tase.build_master_stock_df()
+SLEEP_SECONDS = 30
+POSITION_MAX_TIME = dt.timedelta(minutes=120)
+# ------------------------------
 
-while (1):
-    triggers = get_triggers()
-    if triggers:
-        #open_position(triggered_ticker)
-        # Returns a datetime object containing the local date and time
-        dateTimeObj = datetime.now()
-        print("Position opened at " + dateTimeObj)
-        #track_stock_and_close_position()
-        dateTimeObj = datetime.now()
-        print("Position closed at " + dateTimeObj)
+# ------------ Main ------------
+while(True):
+    maya_msgs = maya.create_msgs_dataframe(stocks_df)       # retrieve MAYA messages
+    maya_msgs = maya.analyze_msgs(maya_msgs)                # get sentiment/score for each message
+    relevant_msgs = maya.filter_relevant_msgs(maya_msgs)    # filter by relevant msgs only
+    if relevant_msgs.count()['ticker'] > 0:
+        for ticker in relevant_msgs['ticker'].unique():     # buy position for each ticker
+            #TODO: check if position is already open for ticker
+            if not restrictions:
+                position_id = ib.buy(ticker)
+                ib.set_stop_loss(position_id)
+                ib.set_take_profit(position_id)
+                restrictions = update_restrictions()
+
+    for position in ib.get_open_positions():
+        if (dt.datetime.now() - position.time > POSITION_MAX_TIME):
+            ib.close_position(position.id)
+
+    time.sleep(SLEEP_SECONDS)
+
+# ------------------------------
 
 
+
+
+# while (1):
+#     triggers = 1
+#     if triggers:
+#         #open_position(triggered_ticker)
+#         # Returns a datetime object containing the local date and time
+#         dateTimeObj = dt.datetime.now()
+#         print("Position opened at " + dateTimeObj)
+#         #track_stock_and_close_position()
+#         dateTimeObj = dt.datetime.now()
+#         print("Position closed at " + dateTimeObj)
 
 
 # While (1):
